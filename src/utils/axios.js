f843e9d39f163ad4d24cd4bdad2c2fdb32ebb4e0
RGABear <russiangamebear@mail.ru>
2024-10-05 19:04:25 +0300

    feat(utils): change API link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,20 +1,21 @@
-import axios from "axios"
-//доп настройки axios 
+import axios from "axios";
+//доп настройки axios
 
-// стандартный путь для отправки запросов 
+// стандартный путь для отправки запросов
 const instance = axios.create({
-    //url
-    //baseURL: "https://admin-vodoley.terexov.ru/api/"
-    //baseURL: "http://127.0.0.1:8000/api/"
-    //baseURL: "http://localhost:5000/"
-    baseURL: "http://94.26.250.179:8000/api/"
-})
+  //url
+  //baseURL: "https://admin-vodoley.terexov.ru/api/"
+  //baseURL: "http://127.0.0.1:8000/api/"
+  //baseURL: "http://localhost:5000/"
+  // baseURL: "http://94.26.250.179:8000/api/"
+  baseURL: "https://admin-vodoley.ocir.ru/api/",
+});
 
-// добавляет токен в хедер запросов 
-instance.interceptors.request.use(config => {
-    const myToken = window.localStorage.getItem('VodoleyToken');
-    config.headers.Authorization = `token ${myToken}`;
-    return config;
-})
+// добавляет токен в хедер запросов
+instance.interceptors.request.use((config) => {
+  const myToken = window.localStorage.getItem("VodoleyToken");
+  config.headers.Authorization = `token ${myToken}`;
+  return config;
+});
 
-export default instance
+export default instance;
